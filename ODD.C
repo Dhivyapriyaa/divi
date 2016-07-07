79e217d4b2df19e0c36d6447180b5c6788ac798d
Dhivyapriyaa <divyaappu001@gmail.com>
2016-07-07 12:22:55 +0530

    Add files via upload

---
+++ ODD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+clrscr();
+printf("enter the number:");
+scanf("%d",&num);
+if(num%2==0)
+{
+printf(" the no is even");
+}
+else
+{
+printf("the no is odd");
+}
+getch();
+}
---
+++ POSNEG.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d", &n);
+if(n > 0)
+{
+printf("positive");
+}
+else if(n < 0)
+{
+printf("negative");
+}
+else
+{
+printf("zero");
+}
+getch();
+}
